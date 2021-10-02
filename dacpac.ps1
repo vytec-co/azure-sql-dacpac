c739a9f7b7c58def90fdc4791d80794533396083
vsm <devops.july2.2017@gmail.com>
2021-10-02 15:21:51 +0530

    Update dacpac.ps1

--- dacpac.ps1
+++ dacpac.ps1
@@ -1,31 +1,32 @@
-$assemblylist =   
-"Microsoft.SqlServer.Management.Common",  
-"Microsoft.SqlServer.Smo",  
-"Microsoft.SqlServer.Dmf ",  
-"Microsoft.SqlServer.Instapi ",  
-"Microsoft.SqlServer.SqlWmiManagement ",  
-"Microsoft.SqlServer.ConnectionInfo ",  
-"Microsoft.SqlServer.SmoExtended ",  
-"Microsoft.SqlServer.SqlTDiagM ",  
-"Microsoft.SqlServer.SString ",  
-"Microsoft.SqlServer.Management.RegisteredServers ",  
-"Microsoft.SqlServer.Management.Sdk.Sfc ",  
-"Microsoft.SqlServer.SqlEnum ",  
-"Microsoft.SqlServer.RegSvrEnum ",  
-"Microsoft.SqlServer.WmiEnum ",  
-"Microsoft.SqlServer.ServiceBrokerEnum ",  
-"Microsoft.SqlServer.ConnectionInfoExtended ",  
-"Microsoft.SqlServer.Management.Collector ",  
-"Microsoft.SqlServer.Management.CollectorEnum",  
-"Microsoft.SqlServer.Management.Dac",  
-"Microsoft.SqlServer.Management.DacEnum",  
-"Microsoft.SqlServer.Management.Utility",
-    "Microsoft.SqlServer.Management.Smo"
-foreach ($asm in $assemblylist)  
-{  
-    $asm = [Reflection.Assembly]::LoadWithPartialName($asm)  
-}  
-    $conn = new-object Microsoft.SqlServer.Management.Common.ServerConnection("{sql server instance name}","{sql user name}","{password}")
+# $assemblylist =   
+# "Microsoft.SqlServer.Management.Common",  
+# "Microsoft.SqlServer.Smo",  
+# "Microsoft.SqlServer.Dmf ",  
+# "Microsoft.SqlServer.Instapi ",  
+# "Microsoft.SqlServer.SqlWmiManagement ",  
+# "Microsoft.SqlServer.ConnectionInfo ",  
+# "Microsoft.SqlServer.SmoExtended ",  
+# "Microsoft.SqlServer.SqlTDiagM ",  
+# "Microsoft.SqlServer.SString ",  
+# "Microsoft.SqlServer.Management.RegisteredServers ",  
+# "Microsoft.SqlServer.Management.Sdk.Sfc ",  
+# "Microsoft.SqlServer.SqlEnum ",  
+# "Microsoft.SqlServer.RegSvrEnum ",  
+# "Microsoft.SqlServer.WmiEnum ",  
+# "Microsoft.SqlServer.ServiceBrokerEnum ",  
+# "Microsoft.SqlServer.ConnectionInfoExtended ",  
+# "Microsoft.SqlServer.Management.Collector ",  
+# "Microsoft.SqlServer.Management.CollectorEnum",  
+# "Microsoft.SqlServer.Management.Dac",  
+# "Microsoft.SqlServer.Management.DacEnum",  
+# "Microsoft.SqlServer.Management.Utility",
+#     "Microsoft.SqlServer.Management.Smo"
+# foreach ($asm in $assemblylist)  
+# {  
+#     $asm = [Reflection.Assembly]::LoadWithPartialName($asm)  
+# }  
+    
+$conn = new-object Microsoft.SqlServer.Management.Common.ServerConnection("{sql server instance name}","{sql user name}","{password}")
 
 $srv = new-object Microsoft.SqlServer.Management.Smo.Server($conn)
 
